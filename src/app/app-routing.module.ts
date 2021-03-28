d544d84e7c4151da23daaca8fb7e4f37807e3872
Eliud Duno <eliudduno@outlook.com>
2021-03-28 12:18:38 -0400

    primer frontend con angular - onlinestore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Agregando rutas
+  {
+    path: 'home',
+    component: DefaultComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'security',
+    loadChildren: () => import('./modules/security/security.module').then(m => m.SecurityModule)
+  },
+  {
+    path: 'parameters',
+    loadChildren: () => import('./modules/parameters/parameters.module').then(m => m.ParametersModule)
+  },
+
+  //** Esta ruta deberia ir de ultima - sino hay ninguna ruta valida esto lo llevara directo al home */
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/security/security-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+
+const routes: Routes = [
+  //**Creando rutas */
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path:'reset',
+    component: PasswordResetComponent
+  },
+  {
+    path:'change-password',
+    component: ChangePasswordComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SecurityRoutingModule { }
